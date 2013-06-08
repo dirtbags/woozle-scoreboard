ca44d0905ccb2f1be84bc679ab641b3d1bf51474
Neale Pickett <neale@woozle.org>
2013-06-08 12:45:18 -0600

    check in some missing files

---
+++ res/adverts.js
@@ -0,0 +1,30 @@
+var advert_num = 100
+var advert_itimer
+
+function adverterror() {
+	if (advert_num > 1) {
+		advert_num = 1
+		rotate_advert()
+	} else {
+		var e = document.getElementById("advert")
+		clearInterval(advert_itimer)
+		e.style.visibility = "hidden"
+	}
+}
+
+function rotate_advert() {
+	var e = document.getElementById("advert")
+	var sn
+	
+	if (advert_num < 10) {
+		sn = "0" + advert_num
+	} else {
+		sn = advert_num
+	}
+	
+	e.src = "ad" + sn + ".jpg"
+	
+	advert_num = advert_num + 1
+}
+
+advert_itimer = setInterval(rotate_advert, 15000)
